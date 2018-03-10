eb4123acf289bb56ef6de8d5f3b8c9af01010df7
wcl <wangcl@longbei.com>
2018-03-10 09:25:25 +0800

    设置页面

---
+++ app/src/main/java/com/old/time/activitys/CircleActivity.java
@@ -0,0 +1,17 @@
+package com.old.time.activitys;
+
+import com.old.time.R;
+
+public class CircleActivity extends CBaseActivity {
+
+    @Override
+    protected int getLayoutID() {
+
+        return R.layout.activity_circle;
+    }
+
+    @Override
+    public void getDataFromNet(boolean isRefresh) {
+
+    }
+}
