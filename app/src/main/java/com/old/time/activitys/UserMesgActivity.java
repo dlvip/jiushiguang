95758fba4cb5953a5f0be56e58def51bfedaca69
wcliang <1371985810@qq.com>
2018-03-12 08:12:47 +0800

    用户信息页面

---
+++ app/src/main/java/com/old/time/activitys/UserMesgActivity.java
@@ -0,0 +1,16 @@
+package com.old.time.activitys;
+
+import com.old.time.R;
+
+public class UserMesgActivity extends BaseActivity {
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected int getLayoutID() {
+        return R.layout.activity_user_mesg;
+    }
+}
