1853171fa37d4129b1b5e4b24a6e7bf3417b0a28
wcl <wangcl@longbei.com>
2018-06-21 18:35:11 +0800

    代码优化

--- app/src/main/java/com/old/time/adapters/UserDressAdapter.java
+++ app/src/main/java/com/old/time/adapters/UserDressAdapter.java
@@ -1,8 +1,17 @@
 package com.old.time.adapters;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
 import com.old.time.R;
+import com.old.time.activitys.CreateDressActivity;
+import com.old.time.dialogs.DialogListManager;
+import com.old.time.interfaces.OnClickManagerCallBack;
+import com.old.time.utils.ActivityUtils;
+import com.old.time.utils.UIHelper;
 
 import java.util.List;
 
@@ -15,11 +24,50 @@ public class UserDressAdapter extends BaseQuickAdapter<String, BaseViewHolder> {
     public UserDressAdapter(List<String> data) {
         super(R.layout.adapter_manager_address, data);
 
-
     }
 
     @Override
     protected void convert(BaseViewHolder helper, String item) {
+        helper.getView(R.id.tv_address_delete).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDeleteDressDialog();
 
+            }
+        });
+        helper.getView(R.id.tv_address_edit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edtDressMessage();
+
+            }
+        });
+    }
+
+    /**
+     * 编辑地址
+     */
+    private void edtDressMessage() {
+        Intent intent = new Intent(mContext, CreateDressActivity.class);
+        ActivityUtils.startActivity((Activity) mContext, intent);
+
+    }
+
+    private DialogListManager mDialogListManager;
+
+    /**
+     * 删除地址
+     */
+    private void showDeleteDressDialog() {
+        if (mDialogListManager == null) {
+            mDialogListManager = new DialogListManager(mContext, new OnClickManagerCallBack() {
+                @Override
+                public void onClickRankManagerCallBack(int typeId, String typeName) {
+                    UIHelper.ToastMessage(mContext, "删除地址");
+
+                }
+            });
+        }
+        mDialogListManager.setDialogViewData("删除地址", new String[]{"删除地址"});
     }
 }
--- app/src/main/java/com/old/time/fragments/HomeFragment.java
+++ app/src/main/java/com/old/time/fragments/HomeFragment.java
@@ -1,12 +1,15 @@
 package com.old.time.fragments;
 
+import android.app.Activity;
 import android.support.v7.widget.GridLayoutManager;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
+import com.chad.library.adapter.base.listener.OnItemClickListener;
 import com.old.time.R;
+import com.old.time.activitys.WebViewActivity;
 import com.old.time.glideUtils.GlideUtils;
 import com.old.time.utils.RecyclerItemDecoration;
 import com.old.time.utils.UIHelper;
@@ -16,7 +19,7 @@ import com.old.time.views.banner.BannerLayout;
  * Created by NING on 2018/3/5.
  */
 
-public class  HomeFragment extends CBaseFragment implements BannerLayout.OnBannerItemClickListener {
+public class HomeFragment extends CBaseFragment {
 
     private BannerLayout recycler_banner;
     private int width;
@@ -30,8 +33,13 @@ public class  HomeFragment extends CBaseFragment implements BannerLayout.OnBanne
         View headerView = View.inflate(mContext, R.layout.header_fragment_home, null);
         recycler_banner = headerView.findViewById(R.id.recycler_banner);
         recycler_banner.initBannerImageView(strings);
-        recycler_banner.setOnBannerItemClickListener(this);
+        recycler_banner.setOnBannerItemClickListener(new BannerLayout.OnBannerItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                WebViewActivity.startWebViewActivity(mContext);
 
+            }
+        });
         mRecyclerView.setLayoutManager(new GridLayoutManager(mContext, 2));
         mRecyclerView.addItemDecoration(new RecyclerItemDecoration(mContext, RecyclerItemDecoration.VERTICAL_LIST, 5));
         mAdapter = new BaseQuickAdapter<String, BaseViewHolder>(R.layout.adapter_home, strings) {
@@ -46,9 +54,15 @@ public class  HomeFragment extends CBaseFragment implements BannerLayout.OnBanne
                 }
                 ImageView img_phone_pic = helper.getView(R.id.img_phone_pic);
                 GlideUtils.getInstance().setImageViewWH(mContext, item, img_phone_pic, width / 2);
-
             }
         };
+        mRecyclerView.addOnItemTouchListener(new OnItemClickListener() {
+            @Override
+            public void onSimpleItemClick(BaseQuickAdapter adapter, View view, int position) {
+                WebViewActivity.startWebViewActivity(mContext);
+
+            }
+        });
         mRecyclerView.setAdapter(mAdapter);
         mAdapter.removeAllHeaderView();
         mAdapter.addHeaderView(headerView);
@@ -58,12 +72,6 @@ public class  HomeFragment extends CBaseFragment implements BannerLayout.OnBanne
     @Override
     public void getDataFromNet(boolean isRefresh) {
         mSwipeRefreshLayout.setRefreshing(false);
-
-    }
-
-    @Override
-    public void onItemClick(int position) {
-        UIHelper.ToastMessage(mContext, TAG + position);
 
     }
 }
