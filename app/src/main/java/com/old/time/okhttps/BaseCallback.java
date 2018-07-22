5abb086a579d8e7b678d21561b0532692421507d
wcliang <1371985810@qq.com>
2018-07-22 23:14:43 +0800

    首页修改

--- app/src/main/java/com/old/time/okhttps/BaseCallback.java
+++ app/src/main/java/com/old/time/okhttps/BaseCallback.java
@@ -4,35 +4,39 @@ import com.lzy.okgo.callback.Callback;
 import com.lzy.okgo.model.Progress;
 import com.lzy.okgo.model.Response;
 import com.lzy.okgo.request.base.Request;
+import com.old.time.utils.DebugLog;
 
 /**
  * Created by wcl on 2018/7/17.
  */
 
-public abstract  class BaseCallback<T> implements Callback<T> {
+public abstract class BaseCallback<T> implements Callback<T> {
+
+    private static final String TAG = BaseCallback.class.getName();
 
     @Override
     public void onStart(Request request) {
-
+        DebugLog.d(TAG, "onStart");
     }
 
     @Override
     public void onCacheSuccess(Response response) {
-
+        DebugLog.d(TAG, "onCacheSuccess");
+        onSuccess(response);
     }
 
     @Override
     public void onFinish() {
-
+        DebugLog.d(TAG, "onFinish");
     }
 
     @Override
     public void uploadProgress(Progress progress) {
-
+        DebugLog.d(TAG, "uploadProgress");
     }
 
     @Override
     public void downloadProgress(Progress progress) {
-
+        DebugLog.d(TAG, "downloadProgress");
     }
 }
