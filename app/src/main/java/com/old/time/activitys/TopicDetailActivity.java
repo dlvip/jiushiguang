753708578ea7da08c59fa6af7a61bf5ebfba26ea
wcl <wangcl@longbei.com>
2018-09-25 18:00:16 +0800

    添加话题详情

---
+++ app/src/main/java/com/old/time/activitys/TopicDetailActivity.java
@@ -0,0 +1,17 @@
+package com.old.time.activitys;
+
+public class TopicDetailActivity extends CBaseActivity {
+
+    @Override
+    protected void initView() {
+        super.initView();
+
+
+    }
+
+    @Override
+    public void getDataFromNet(boolean isRefresh) {
+
+
+    }
+}
