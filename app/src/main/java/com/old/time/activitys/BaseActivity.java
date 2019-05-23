94cf167d05dad704ac2d3355a072ccbf4b01ce57
wcl <wangcl@longbei.com>
2019-05-23 17:57:39 +0800

    优化权限设置

--- app/src/main/java/com/old/time/activitys/BaseActivity.java
+++ app/src/main/java/com/old/time/activitys/BaseActivity.java
@@ -1,22 +1,15 @@
 package com.old.time.activitys;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
-import android.net.Uri;
-import android.os.Build;
-import android.os.Bundle;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.LayoutRes;
-import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.old.time.R;
-import com.old.time.dialogs.DialogPromptCentre;
-import com.old.time.interfaces.OnClickViewCallBack;
+import com.old.time.permission.PermissionUtil;
 import com.old.time.utils.ActivityUtils;
 import com.old.time.utils.ScreenTools;
 import com.old.time.utils.UIHelper;
@@ -27,17 +20,16 @@ import com.umeng.socialize.UMShareAPI;
  * Created by NING on 2018/2/23.
  */
 
-public abstract class BaseActivity extends AppCompatActivity implements View.OnClickListener {
-
-    public Activity mContext;
-    public String TAG;
+public abstract class BaseActivity extends PermissionActivity implements View.OnClickListener {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        this.TAG = getClass().getName();
-        this.mContext = this;
-        setOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    protected void loadView() {
+        String[] mPermissions = getNeedPermissions();
+        if (mPermissions != null && mPermissions.length > 0 //
+                && !PermissionUtil.checkAndRequestPermissionsInActivity(mContext, mPermissions)) {
+
+            return;
+        }
         setContentView(getLayoutID());
         initView();
         initEvent();
@@ -105,41 +97,6 @@ public abstract class BaseActivity extends AppCompatActivity implements View.OnC
 
     }
 
-    private DialogPromptCentre mDialogPromptCentre;
-
-    /**
-     * 申请权限的弹框
-     */
-    protected void showDialogPermission() {
-        if (mDialogPromptCentre == null) {
-            mDialogPromptCentre = new DialogPromptCentre(mContext, new OnClickViewCallBack() {
-                @Override
-                public void onClickTrueView() {
-                    Intent localIntent = new Intent();
-                    localIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    if (Build.VERSION.SDK_INT >= 9) {
-                        localIntent.setAction("android.settings.APPLICATION_DETAILS_SETTINGS");
-                        localIntent.setData(Uri.fromParts("package", getPackageName(), null));
-
-                    } else if (Build.VERSION.SDK_INT <= 8) {
-                        localIntent.setAction(Intent.ACTION_VIEW);
-                        localIntent.setClassName("com.android.settings", "com.android.settings.InstalledAppDetails");
-                        localIntent.putExtra("com.android.settings.ApplicationPkgName", getPackageName());
-
-                    }
-                    ActivityUtils.startActivity(mContext, localIntent);
-
-                }
-
-                @Override
-                public void onClickCancelView() {
-
-                }
-            });
-        }
-        mDialogPromptCentre.showDialog("申请权限失败，请手动开启相关权限");
-    }
-
     /**
      * 获得状态栏高度
      */
@@ -173,15 +130,9 @@ public abstract class BaseActivity extends AppCompatActivity implements View.OnC
 
     }
 
-    public void setOrientation(int orientation) {
-        setRequestedOrientation(orientation);
-
-    }
-
     protected abstract void initView();
 
-    protected abstract @LayoutRes
-    int getLayoutID();
+    protected abstract @LayoutRes int getLayoutID();
 
     private int W, H;
     private int showX, showY;
@@ -233,5 +184,4 @@ public abstract class BaseActivity extends AppCompatActivity implements View.OnC
         UMShareAPI.get(this).onActivityResult(requestCode, resultCode, data);
 
     }
-
 }
---
+++ app/src/main/java/com/old/time/activitys/PermissionActivity.java
@@ -0,0 +1,100 @@
+package com.old.time.activitys;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+
+import com.old.time.dialogs.DialogPromptCentre;
+import com.old.time.interfaces.OnClickViewCallBack;
+import com.old.time.permission.PermissionUtil;
+import com.old.time.utils.ActivityUtils;
+
+/**
+ * Created by NING on 2019/2/23.
+ */
+public abstract class PermissionActivity extends AppCompatActivity implements PermissionUtil.PermissionCallBack {
+
+    public Activity mContext;
+    public String TAG;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.TAG = getClass().getName();
+        this.mContext = this;
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        loadView();
+
+    }
+
+    /**
+     * 初始化页面
+     */
+    protected abstract void loadView();
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        PermissionUtil.onPermissionResult(this, permissions, grantResults, this);
+
+    }
+
+    private DialogPromptCentre mDialogPromptCentre;
+
+    /**
+     * 申请权限的弹框
+     */
+    protected void showDialogPermission() {
+        if (mDialogPromptCentre == null) {
+            mDialogPromptCentre = new DialogPromptCentre(mContext, new OnClickViewCallBack() {
+                @Override
+                public void onClickTrueView() {
+                    Intent localIntent = new Intent();
+                    localIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    localIntent.setAction("android.settings.APPLICATION_DETAILS_SETTINGS");
+                    localIntent.setData(Uri.fromParts("package", getPackageName(), null));
+                    ActivityUtils.startActivity(mContext, localIntent);
+
+                }
+
+                @Override
+                public void onClickCancelView() {
+
+                }
+            });
+        }
+        mDialogPromptCentre.showDialog("申请权限失败，请手动开启相关权限");
+    }
+
+    /**
+     * 权限
+     *
+     * @return
+     */
+    protected String[] getNeedPermissions() {
+
+        return new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+    }
+
+    @Override
+    public void onSuccess() {
+        loadView();
+
+    }
+
+    @Override
+    public void onShouldShow() {
+
+    }
+
+    @Override
+    public void onFailed() {
+        showDialogPermission();
+
+    }
+}
